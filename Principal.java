c1721fde96aea4784ee0d9fa307cb3e365c7782b
Mihaela Corina <phoenixco@users.noreply.github.com>
2016-03-16 22:48:14 +0100

    Added files via upload

---
+++ Principal.java
@@ -0,0 +1,181 @@
+package realidad;
+
+import java.util.Scanner;
+/**
+ * Clase principal.Trabaja con las clases Persona (introduciendo los datos de dos personas) y Realidad.
+ * 
+ * @Javier & Gregorio & Alvaro & Samuel & Mihaela
+ * @16.03.2015
+ */
+public class Principal
+{
+    public static void main(){
+        Scanner sc= new Scanner(System.in);
+        String nombre,nombre1,percepciondemas,mundo,emocion,percepcion,entrada="";
+        String percepciondemas1,mundo1,emocion1,percepcion1;
+        int edad,edad1,opcion=1;
+             
+        System.out.printf("Introduce los datos de una persona.\n");
+        System.out.printf("_______________________________________________________________________________________________________________________\n");
+        System.out.printf("Introduce el nombre: ");
+        nombre=sc.nextLine();
+        System.out.printf("Introduce la edad: ");
+        edad=sc.nextInt();
+             
+        sc.nextLine();
+        System.out.printf("Introduce el estado emocional [DEPRIMIDO || FELIZ || FEERICO]: ");
+        emocion=sc.nextLine();
+              
+        System.out.printf("Introduce la percepción de si mismo [MALA|| NORMAL || BUENA]: ");
+        percepcion=sc.nextLine();
+        System.out.printf("Introduce la percepción de los demas [ENEMIGOS|| NORMALES|| AMIGABLES]:");
+        percepciondemas=sc.nextLine();
+        Persona persona1=new Persona(nombre, edad, emocion, percepcion, percepciondemas);
+        System.out.printf("_______________________________________________________________________________________________________________________\n");
+        System.out.printf("Introduce los datos de otra persona\n ");
+        System.out.printf("Introduce el nombre: ");
+        nombre1=sc.nextLine();
+        System.out.printf("Introduce la edad: ");
+        edad1=sc.nextInt();
+              
+        sc.nextLine();
+        System.out.printf("Introduce el estado emocional [DEPRIMIDO|| FELIZ|| FEERICO]:");
+        emocion1=sc.nextLine();
+             
+        System.out.printf("Introduce la percepción de si mismo [MALA || NORMAL || BUENA]:");
+        percepcion1=sc.nextLine();
+        System.out.printf("Introduce la percepción de los demas [ENEMIGOS || NORMALES || AMIGABLES]:");
+        percepciondemas1=sc.nextLine();
+        Persona persona2=new Persona(nombre1, edad1, emocion1,  percepcion1, percepciondemas1);
+              
+        System.out.printf("***********************************************************************************************\n");
+    
+        do{
+            System.out.printf("Introduce una opcion.\n ");
+            System.out.printf("1. Si quieres los datos de  [ %s ] y de [%s].\n ",nombre, nombre1);
+            System.out.printf("2. Si quieres saber si [ %s ] es optimista o pesimista.\n ",nombre);
+            System.out.printf("3. Si quieres saber si[  %s ] es optimista o pesimista.\n ",nombre1);
+            System.out.printf("4. Si %s con [ %s ] han interactuado.\n ",nombre,nombre1);
+            opcion=sc.nextInt();sc.nextLine();
+            switch(opcion){
+                case 1:System.out.printf("La realidad personal de: %s\n", persona1.getPersona().getNombre());
+                       System.out.printf("Mundo:  %s \n", persona1.getPersona().getMundo());
+                              
+                       System.out.printf("Esta [%s ] \n", Principal.estEmo( persona1.getPersona().getestadoEmocional()));
+                       System.out.printf("%s : Ve a los demas [ %s] \n ",nombre, Principal.perDemas( persona1.getPersona().getestadoEmocional()));
+                       System.out.printf("%s :Tiene una percepción [ %s ] de si mismo\n",nombre, Principal.perMismo( persona1.getPersona().getestadoEmocional()));
+                              
+                       System.out.printf("***********************************************************************************************\n");
+                              
+                       System.out.printf("La realidad personal de: %s\n", persona2.getPersona().getNombre());
+                       System.out.printf("Mundo: %s\n", persona2.getPersona().getMundo());
+                              
+                       System.out.printf("%s : Esta %s \n", nombre1,Principal.estEmo( persona2.getPersona().getestadoEmocional()));
+                       System.out.printf("%s : Ve a los demas  %s\n ",nombre1, Principal.perDemas( persona2.getPersona().getestadoEmocional()));
+                       System.out.printf("%s :Tiene una percepción %s de si mismo \n",nombre1, Principal.perMismo( persona2.getPersona().getestadoEmocional()));
+
+                System.out.printf("_______________________________________________________________________________________________________________________\n");
+                       break;
+               
+                case 2:if(persona1.realidadOptimista(persona1.getPersona())){
+                          System.out.printf(" %s  Es una persona optimista\n",nombre);      
+                          }else{
+                          System.out.printf("%s Es una persona pesimista y necesita un [ golpe ] de energia positiva\n",nombre); 
+                       }
+                       break;
+                       
+                case 3:if(persona2.realidadOptimista(persona2.getPersona())){
+                          System.out.printf(" %s  Es una persona optimista\n",nombre1);      
+                       }else{
+                          System.out.printf("%s Es una persona pesimista y necesita un [ golpe ] de energia positiva\n",nombre1); 
+                       }
+                       break;
+                case 4:persona1.interactuar(persona2.getPersona());
+        
+                System.out.printf("\n************************************************\n");
+                System.out.printf("Las realidades personales despues de interactuar\n");                       
+                System.out.printf("***********************************************************************************************\n");
+                    
+                System.out.printf("La realidad personal de  %s\n :", nombre);
+                System.out.printf("Mundo: %s\n", persona1.getPersona().getMundo());
+                System.out.printf(" %s : Esta %s \n", nombre,Principal.estEmo( persona1.getPersona().getestadoEmocional()));
+                System.out.printf("%s : Ve a los demas  %s\n ", nombre,Principal.perDemas( persona1.getPersona().getestadoEmocional()));
+                System.out.printf("%s : Tiene una percepcion %sde si mismo \n", nombre,Principal.perMismo( persona1.getPersona().getestadoEmocional()));
+                      
+                System.out.printf("\n");
+                                     
+                System.out.printf("La realidad personal de %s\n :", nombre1);
+                System.out.printf("Mundo: %s\n", persona2.getPersona().getMundo());
+                System.out.printf(" %s : Esta %s\n", nombre1,Principal.estEmo( persona2.getPersona().getestadoEmocional()));
+                System.out.printf(" %s :Ve a los demas  %s\n ", nombre1,Principal.perDemas( persona2.getPersona().getestadoEmocional()));
+                System.out.printf(" %s :Tiene una percepcion %s de si mismo \n", nombre1,Principal.perMismo( persona2.getPersona().getestadoEmocional()));
+
+                default:System.out.printf("No es una opción corecta"); break; 
+            }
+            System.out.printf("Quieres salir del programa? si/no\n ");
+            entrada=sc.nextLine();
+            System.out.printf("Pulsa enter para continuar\n");sc.nextLine();
+        }while(!entrada.equals("si"));
+        System.out.printf("Fin del programa\n ");
+    }
+            
+    /**
+     * Método para asignar un valor  el estado emocional recibido por parámetro.
+     * 
+     * @param int- estado emocional
+     * @return String - estado emocional
+     */
+    public static String estEmo(int em){
+        String emocion="";
+        if(em==Realidad.DEPRIMIDO){
+            System.out.printf(emocion="DEPRIMIDO " );
+        }else{ 
+            if(em==Realidad.FELIZ){               
+              System.out.printf(emocion=" FELIZ " );
+            }else{
+               System.out.printf(emocion=" FEERICO " );
+            }                    
+        }
+        return emocion;
+    }
+    
+    /**
+     * Método para asignar un valor  a la percepción de los demas recibida por parametro.
+     * 
+     * @param int- percepción de los demas
+     * @return String - percepción de los demas
+     */
+    public static String perDemas(int per){
+        String percep="";
+        if(per==Realidad.ENEMIGOS){
+            System.out.printf(percep="ENEMIGOS" );
+        }else{ 
+           if(per==Realidad.NORMALES){
+              System.out.printf(percep="NORMALES " );
+           }else{
+              System.out.printf(percep="AMIGABLES" );
+           }
+        }
+        return percep;
+    }
+    
+    /**
+     * Método para asignar un valor a la percepción personal.
+     * 
+     * @param int- percepcion personal
+     * @return String - percepcion personal
+     */
+    public static String perMismo(int per){
+        String percemismo="";
+        if(per==Realidad.MALA){
+           System.out.printf(percemismo="MALA" );
+        }else{ 
+           if(per==Realidad.NORMAL){
+              System.out.printf(percemismo="NORMAL" );
+           }else{
+              System.out.printf(percemismo="BUENA" );
+           }
+        }                
+        return percemismo;
+    }
+}
---
+++ Realidad.java
@@ -0,0 +1,215 @@
+package realidad;
+
+/**Clase que encapsula un objeto realidad .
+ * 
+ * @Javier &Gregorio &Alvaro &Samuel& Mihaela
+ * @ 16.03.2015
+ */
+public class Realidad
+{
+    private String nombre,mundo;
+    private int percepcionDeSiMismo,percepcionDelosDemas,estadoEmocional;
+    
+    public static final int ENEMIGOS=0;
+    public static final int NORMALES=1;
+    public static final int AMIGABLES=2;
+    
+    public static final int MALA=3;
+    public static final int NORMAL=4;
+    public static final int BUENA=5;
+    
+    public static final int DEPRIMIDO=6;
+    public static final int FELIZ=7;
+    public static final int FEERICO=8;
+   
+   
+   
+    /**
+     * Constructor por defecto.Asigna unos valores por defecto al objeto de tipo realidad .
+     */
+    public Realidad() {
+        this("Pepe","feliz","normal","amigables");
+    }
+    /**
+     * Constructor de un objeto realidad a partir de los parametros recibidos.
+     * 
+     * @param nombre - nombre del objeto
+     * @param emocional - estado emocional
+     * @param per - percepcion de si mismo
+     * @param demas - percepcion de los demas
+     */
+    public Realidad(String nombre, String emocional,  String per, String perDemas){
+        this.nombre=nombre;
+        this.setPercepcionDemas(perDemas);
+        this.setpercepcionDeSiMismo(per);
+        setestadoEmocional(emocional);
+        setMundo();
+    }
+    
+    /**
+     * Método asigna al objeto de tipo realidad el valor que recibe por parametro.[ENEMIGOS||NORMALES||AMIGABLES]
+     * 
+     * @param per - percepcion de los demas
+     */
+    public void setPercepcionDemas(String per){
+        per=per.toLowerCase();
+        switch(per){
+            case "enemigos": percepcionDelosDemas=ENEMIGOS; break;
+            case "normales": percepcionDelosDemas=NORMALES; break;
+            case "amigables": percepcionDelosDemas=AMIGABLES; break;
+            default: break;
+        }
+    }
+    
+    /**
+     * Método asigna al objeto de tipo realidad el valor que recibe por parametro.[MALA||NORMAL||BUENA]
+     * 
+     * @param perD - percepcion de si mismo
+     */
+    public void setpercepcionDeSiMismo(String perD){
+        perD=perD.toLowerCase();
+        switch(perD){
+            case "mala": percepcionDeSiMismo=MALA; break;
+            case "normal": percepcionDeSiMismo=NORMAL; break;
+            case "buena": percepcionDeSiMismo=BUENA; break;
+            default: break;
+        }
+    }
+    
+    /**
+     * Método asigna al objeto de tipo realidad el valor que recibe por parametro.[DEPRIMIDO||FELIZ||FEERICO]
+     * 
+     * @param emocion - estado emocional.
+     */
+    public void setestadoEmocional(String emocion){
+        emocion=emocion.toLowerCase();
+        switch(emocion){
+            case "deprimido": estadoEmocional=DEPRIMIDO; break;
+            case "feliz": estadoEmocional=FELIZ; break;
+            case "feerico": estadoEmocional=FEERICO; break;
+            default: break;
+        }
+    }
+    /**
+     * Método asigna un valor al objeto de tipo realidad . * 
+     */
+    public void setMundo(){
+        if(this.estadoEmocional==FEERICO){
+            this.mundo="Utopia";
+        }else if(this.estadoEmocional==FELIZ){
+            this.mundo="Feliz";
+        }else{
+            this.mundo="Gris";
+        }
+    }
+    
+    /**
+     * Método asigna al objeto de tipo realidad los valores que recibe por parametro.
+     * 
+     * @param per - percepcion de los demas
+     * @param per - percepcion de los demas
+     * @param per - percepcion de si mismo
+     * @param perDemas - percepcion de los demas
+     */
+    public void setRealidad(String nombre, String emocional, String per, String perDemas){
+        this.nombre=nombre;
+        setestadoEmocional(emocional);
+        setPercepcionDemas(perDemas);
+        setpercepcionDeSiMismo(per);
+        setMundo();
+    }
+    
+    /**
+     * Método para obtener el nombre de la realidad.
+     * 
+     * @return String - nombre;
+     */
+    public String getNombre(){
+        return nombre;    
+    }
+    
+    /**
+     * Método para obtener el mundo de la realidad.
+     * 
+     * @return String - mundo;
+     */
+    public String getMundo(){
+        return mundo;    
+    }
+    /**
+     * Método para obtener el estado emocional.
+     * 
+     * @return String - estado emocional;
+     */
+    public int getestadoEmocional(){
+        return this.estadoEmocional;    
+    }
+    /**
+     * Método para obtener la percepcion de los demas.
+     * 
+     * @return String - percepcion de los demas.
+     */
+    public int getpercepcionDelosDemas(){
+        return percepcionDelosDemas;    
+    }
+    
+    /**
+     * Método para obtener la percepcion de si mismo.
+     * 
+     * @return String - percepcion de si mismo.
+     */
+    public int getpercepcionDeSiMismo(){    
+       return percepcionDeSiMismo;    
+    }
+    
+    /**
+     * Método para obtener modificar el objeto de tipo realidad con una realidad recibida por parametro.
+     * 
+     * @param Realidad - realidad.
+     */
+    public void modificarRealidad(Realidad reality){ 
+        if(this.percepcionDeSiMismo>reality.percepcionDeSiMismo){            
+            this.percepcionDeSiMismo--;        
+        }else if(this.percepcionDeSiMismo<reality.percepcionDeSiMismo){            
+            this.percepcionDeSiMismo++;        
+        }
+        
+        if(this.percepcionDelosDemas>reality.percepcionDelosDemas){            
+            this.percepcionDelosDemas--;        
+        }else if(this.percepcionDelosDemas<reality.percepcionDelosDemas){            
+            this.percepcionDelosDemas++;        
+        }
+        
+        if(this.estadoEmocional>reality.estadoEmocional){            
+            this.estadoEmocional--;        
+        }else if(this.estadoEmocional<reality.estadoEmocional){            
+            this.estadoEmocional++;        
+        }       
+    }
+    
+    /**
+     * Método para obtener la realidad recibida por parametro es optimista o no .
+     * 
+     * @return boolean  - true/false;
+     */
+    public boolean esOptimista(Realidad reality){
+       boolean sw=false;
+       if(reality.percepcionDeSiMismo==BUENA||reality.percepcionDelosDemas==AMIGABLES||reality.estadoEmocional==FEERICO){
+          sw=true;
+       } 
+       return sw;
+    }
+    /**
+     * Método para obtener la realidad recibida por parametro es optimista o no .
+     * 
+     * @return boolean  - true/false;
+     * 
+     */
+    public boolean esOptimista(){
+       boolean sw=false;
+       if(this.percepcionDeSiMismo==BUENA||this.percepcionDelosDemas==AMIGABLES||this.estadoEmocional==FEERICO){
+          sw=true;
+       }
+       return sw;
+    }
+}
